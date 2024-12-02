f6308ea95f93dfabad5c237b6c82e5decc6e7b61
Dmitry Yerusalimtsev <Dima.Yerusalimtsev@gmail.com>
2024-12-02 18:20:40 +0200

    Update Main.scala

--- asani-scala/src/test/scala/com/dyeru/asani/arrow/Main.scala
+++ asani-scala/src/test/scala/com/dyeru/asani/arrow/Main.scala
@@ -7,13 +7,7 @@ case class Frame(image: Array[Byte], metadata: String, timestamp: Long)
 
 @main
 def main(): Unit = {
-  //  val schema = CaseClassToArrowSchema.toArrowSchema[Frame]
-  //  println(schema)
   case class User(id: Int, name: String, nick: Option[String], bytes: Array[Byte])
-
-//  val s = new ArrowSchema {}.schemaOf[User]
-//  println(s)
-
   case class Person(name: String, age: Int, active: Boolean)
 
   val allocator = new RootAllocator(Long.MaxValue)
